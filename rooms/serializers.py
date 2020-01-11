4ebfb65a56d7aade453ab0a00cf263c5ec1c62a6
dev-jaesim <dev.jaesim@gmail.com>
2020-01-12 00:16:04 +1100

    View sets done

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -9,4 +9,12 @@ class RoomSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Room
-        fields = ("name", "price", "instant_book", "user")
+        fields = ("pk", "name", "price", "instant_book", "user")
+
+
+class BigRoomSerializer(serializers.ModelSerializer):
+    user = TinyUserSerializer()
+
+    class Meta:
+        model = Room
+        fields = "__all__"
