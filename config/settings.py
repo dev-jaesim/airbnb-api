55372282bf07ef232c40bc5ec6f00a5e68a36d70
dev-jaesim <dev.jaesim@gmail.com>
2020-01-25 13:25:18 +1100

    Listing, retrieving, updating, creating and deleting rooms with modelviewset

--- config/settings.py
+++ config/settings.py
@@ -139,7 +139,7 @@ AUTH_USER_MODEL = "users.User"
 # Django Rest Framework
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 1,
+    "PAGE_SIZE": 2,
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "config.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
